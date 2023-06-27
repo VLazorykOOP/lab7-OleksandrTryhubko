60fa7702c80650586ce9f41e8320be2a62d7aa0b
OleksandrTryhubko <124136144+OleksandrTryhubko@users.noreply.github.com>
2023-06-28 00:28:36 +0300

    Add files via upload

--- OOPLab7T/OOPLab7T.cpp
+++ OOPLab7T/OOPLab7T.cpp
@@ -1,23 +1,118 @@
-﻿// OOPLab7T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
 
-#include <iostream>
-#include <clocale>
+using namespace std;
+
+template<typename T>
+void swapValues(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template<typename T>
+void selectionSort(T arr[], int size)
+{
+    for (int i = 0; i < size - 1; ++i)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            swapValues(arr[i], arr[minIndex]);
+        }
+    }
+}
+
+template<>
+void selectionSort<const char*>(const char* arr[], int size)
+{
+    for (int i = 0; i < size - 1; ++i)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; ++j)
+        {
+            if (strcmp(arr[j], arr[minIndex]) < 0)
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            swapValues(arr[i], arr[minIndex]);
+        }
+    }
+}
 
 int main()
 {
-    setlocale(LC_CTYPE, "ukr");
-    std::cout << "Лабораторна робота №7  \n";
-    std::cout << "Автоматизована система ...  \n";
+    // Введення даних з клавіатури
+    int num1, num2;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Initial values: " << num1 << ", " << num2 << endl;
+    swapValues(num1, num2);
+    cout << "Changed values: " << num1 << ", " << num2 << endl;
+
+    // Введення даних з файлу
+    ifstream inputFile("input.txt");
+    if (inputFile.is_open())
+    {
+        int fileNum1, fileNum2;
+        inputFile >> fileNum1 >> fileNum2;
+
+        cout << "Initial values from file: " << fileNum1 << ", " << fileNum2 << endl;
+        swapValues(fileNum1, fileNum2);
+        cout << "Changed values from file: " << fileNum1 << ", " << fileNum2 << endl;
+
+        inputFile.close();
+    }
+    else
+    {
+        cout << "Error opening file." << endl;
+    }
+
+    // Введення даних за допомогою датчика випадкових чисел
+    srand(time(0)); // Ініціалізація генератора випадкових чисел
+    int randNum1 = rand();
+    int randNum2 = rand();
+
+    cout << "Initial random values: " << randNum1 << ", " << randNum2 << endl;
+    swapValues(randNum1, randNum2);
+    cout << "Changed random values: " << randNum1 << ", " << randNum2 << endl;
+
+    // Впорядкування масиву типу const char*
+    const int charArraySize = 5;
+    const char* charArray[charArraySize] = { "orange", "apple", "banana", "grape", "pear" };
+
+    cout << "Initial char array values: ";
+    for (int i = 0; i < charArraySize; ++i)
+    {
+        cout << charArray[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(charArray, charArraySize);
+
+    cout << "Sorted char array values: ";
+    for (int i = 0; i < charArraySize; ++i)
+    {
+        cout << charArray[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
